95b7697ac954b8ec740447bdb45711718faf2ead
wanglei.breath <wanglei.breath@bytedance.com>
2024-12-10 14:51:04 +0800

    feat: 测试

---
+++ index.js
@@ -0,0 +1,10 @@
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.end('hello world');
+// }).listen(3000, () => {
+//     console.log('listen 3000');
+// });
+
+console.log('****************************** test1');
+console.log('****************************** test2');
+console.log('****************************** test3');
